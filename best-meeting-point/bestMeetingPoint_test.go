d437ac64809348ac881d5b3a227d588e44eebc0c
Melbex De Leon <melbex11@gmail.com>
2021-09-15 16:30:42 +0800

    Add test cases

--- best-meeting-point/bestMeetingPoint_test.go
+++ best-meeting-point/bestMeetingPoint_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMinTotalDistance(t *testing.T) {
+func TestMinTotalDistance1(t *testing.T) {
 	testGrid := [][]int{
 		{1, 0, 0, 0, 1},
 		{0, 0, 0, 0, 0},
@@ -13,6 +13,28 @@ func TestMinTotalDistance(t *testing.T) {
 	}
 
 	expectedAnswer := 6
+	ans := minTotalDistance(testGrid)
+
+	assert.Equal(t, expectedAnswer, ans)
+}
+
+func TestMinTotalDistance2(t *testing.T) {
+	testGrid := [][]int{
+		{1, 0, 0, 0, 0, 0, 0, 1, 1},
+	}
+
+	expectedAnswer := 8
+	ans := minTotalDistance(testGrid)
+
+	assert.Equal(t, expectedAnswer, ans)
+}
+
+func TestMinTotalDistance3(t *testing.T) {
+	testGrid := [][]int{
+		{1, 1, 0, 0, 1, 1},
+	}
+
+	expectedAnswer := 8
 	ans := minTotalDistance(testGrid)
 
 	assert.Equal(t, expectedAnswer, ans)
